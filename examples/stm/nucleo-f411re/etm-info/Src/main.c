baa7bcea36d1f873f1a73099d4ece8e1bd5a2ea9
Dan McLeran <danmcleran@gmail.com>
2025-11-18 08:45:12 -0700

    Add xtract to source and put in a trivial API call.

--- examples/stm/nucleo-f411re/etm-info/Src/main.c
+++ examples/stm/nucleo-f411re/etm-info/Src/main.c
@@ -17,6 +17,7 @@
  */
 
 #include <stdint.h>
+#include "xtract.h"
 
 #if !defined(__SOFT_FP__) && defined(__ARM_FP)
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
@@ -24,6 +25,8 @@
 
 int main(void)
 {
+	xtract_etm_init(0);
+
     /* Loop forever */
 	for(;;);
 }
